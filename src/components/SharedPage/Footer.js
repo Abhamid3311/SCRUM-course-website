9a606c0d49a2b659e83f24fa500cb2a73188fede
Hamid <www.abdulhamid3311@gmail.com>
2022-06-29 15:31:34 +0600

    added gooter

--- src/components/SharedPage/Footer.js
+++ src/components/SharedPage/Footer.js
@@ -1,32 +1,60 @@
 import React from 'react';
-import { BsFacebook, BsYoutube, BsTwitter, BsInstagram, BsWhatsapp } from 'react-icons/bs';
+import { BsFacebook, BsYoutube, BsTwitter, BsInstagram, BsWhatsapp, BsFillEnvelopeFill } from 'react-icons/bs';
+import { BiPhoneCall } from 'react-icons/bi';
 
 const Footer = () => {
     return (
         <div className='bg-gray-800 text-white'>
-            <footer class="footer p-10  ">
-                <div>
-                    <span class="footer-title">Services</span>
-                    <a class="link link-hover">Branding</a>
-                    <a class="link link-hover">Design</a>
-                    <a class="link link-hover">Marketing</a>
-                    <a class="link link-hover">Advertisement</a>
+            <footer class="footer p-10 ">
+                <div className='border-r pr-8'>
+                    <h2 className='text-3xl font-bold mb-3'>SCRUMVERSE</h2>
+                    <p>We ara a lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun.</p>
+                    <div className='flex flex-row items-center justify-between mt-5'>
+                        <div className='flex flex-row items-center mr-12'>
+                            <BiPhoneCall className='text-4xl mr-3'></BiPhoneCall>
+                            <div>
+                                <p>Have a question?</p>
+                                <p className='text-xl'>310-437-2766</p>
+                            </div>
+                        </div>
+                        <div className='flex flex-row items-center '>
+                            <BsFillEnvelopeFill className='text-4xl mr-3'></BsFillEnvelopeFill>
+                            <div>
+                                <p>Contact us at</p>
+                                <p className='text-xl'>unreal@outlook.com</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <span class="footer-title">Company</span>
-                    <a class="link link-hover">About us</a>
-                    <a class="link link-hover">Contact</a>
-                    <a class="link link-hover">Jobs</a>
-                    <a class="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span class="footer-title">Legal</span>
-                    <a class="link link-hover">Terms of use</a>
-                    <a class="link link-hover">Privacy policy</a>
-                    <a class="link link-hover">Cookie policy</a>
+                <div className='pl-10'>
+                    <span class="footer-title">Newsletter</span>
+                    <div class="form-control w-full">
+                        <label class="label">
+                            <span >Be the first one to know  about discounts, offers and events weekly in your mailbox. Unsubscribe whenever you like with one click.</span>
+                        </label>
+                        <div class="relative">
+                            <input type="text" placeholder="enter your email" class="input input-bordered w-full bg-gray-700 pr-16" />
+                            <button class="btn bg-indigo-500 absolute top-0 right-0 rounded-l-none">Submit</button>
+                        </div>
+                    </div>
                 </div>
             </footer>
-            <footer class="footer px-10 py-4 border-t flex flex-row-reverse  border-base-300 justify-between">
+
+            <footer class="footer p-10  ">
+                <div class="grid grid-flow-col pt-10 gap-4 w-full border-t  py-4 ">
+                    <a class="link link-hover">About us</a>
+                    <a class="link link-hover">Jobs</a>
+                    <a class="link link-hover">Blog</a>
+                    <a class="link link-hover">Press</a>
+                    <a class="link link-hover">FAQ</a>
+                    <a class="link link-hover">Career</a>
+                    <a class="link link-hover">Contact</a>
+                    <a class="link link-hover">Privacy policy</a>
+                    <a class="link link-hover">Sitemap</a>
+                    <a class="link link-hover">Terms of use</a>
+                </div>
+            </footer>
+            <footer class="footer px-10  flex flex-row-reverse  border-base-300 justify-between">
                 <div class="items-center grid-flow-col">
                     <p>© 2000-2022 | All right reserved </p>
                 </div>
