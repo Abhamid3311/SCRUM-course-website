fa81d211cacfe6f38d41fae0d45b134a94192846
Hamid <www.abdulhamid3311@gmail.com>
2022-06-30 14:12:43 +0600

    added badge functinality

--- src/components/HomePages/Course.js
+++ src/components/HomePages/Course.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { BsStarFill, BsStarHalf } from 'react-icons/bs';
+import React, { useState } from 'react';
+import { BsStarFill, BsStarHalf, BsBookmarkHeart } from 'react-icons/bs';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 
 const Course = ({ course }) => {
     const { id, name, instructor, ratings, img } = course;
+    const [marked, setMarked] = useState(false);
+
+    const handleBookMark = (id) => {
+        setMarked(!marked)
+    };
+
     return (
 
         <div className="card w-50  card-compact shadow-lg hover:shadow-2xl">
             <figure><img src={img} alt="course" /></figure>
             <div className="card-body">
-                <h2 className="card-title text-xl">{name}</h2>
+                <div className='flex flex-row justify-between items-top'>
+                    <h2 className="card-title text-xl">{name}</h2>
+                    <BsBookmarkHeart onClick={() => handleBookMark(id)}
+                        className={(marked ? 'text-red-500 text-5xl' : 'text-black text-5xl')}
+                    ></BsBookmarkHeart>
+
+                </div>
                 <p className='text-muted text-lg'>{instructor}</p>
                 <div className='flex flex-row justify-start items-center text-lg text-indigo-500'>
                     <span className='mr-3 font-bold'>{ratings} </span>
