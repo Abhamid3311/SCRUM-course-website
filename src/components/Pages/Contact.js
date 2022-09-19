673fdc4128aa874d5508ab9fa6bb068e671163f4
Hamid <www.abdulhamid3311@gmail.com>
2022-09-19 14:23:33 +0600

    added contact us

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -3,7 +3,35 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            Contact
+            <div className="hero min-h-screen bg-indigo-100 px-10">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Contact Us!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Message</span>
+                                </label>
+                                <textarea type="text" placeholder="Message" className="textarea textarea-bordered" />
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Send</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
